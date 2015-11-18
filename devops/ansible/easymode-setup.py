058bab1ab41b7485dc7352fc45b1028aeb2d3afb
Luke J Harmon <lukeh@uidaho.edu>
2015-11-18 08:30:51 -0800

    files to make PGLS app work

--- devops/ansible/easymode-setup.py
+++ devops/ansible/easymode-setup.py
@@ -1,4 +1,4 @@
-from GirderClient import *
+from girder_client import GirderClient
 import json
 import pymongo
 import sys
@@ -8,28 +8,80 @@ if len(sys.argv) < 2:
     sys.exit(1)
 arborWebAppsPath = sys.argv[1]
 
-# Read our analyses into Python dictionaries.
-ACR = {}
-with open ("%s/ancestral-state/aceArbor.json" % arborWebAppsPath, "r") as acrFile:
-    acrStr = acrFile.read()
-ACR['analysis'] = json.loads(acrStr)
-
-PGS = {}
-with open ("%s/phylogenetic-signal/Phylogenetic_signal.json" % arborWebAppsPath, "r") as pgsFile:
-    pgsStr = pgsFile.read()
-PGS['analysis'] = json.loads(pgsStr)
-
 # Get the ID for our Analyses folder.
-c = GirderClient('localhost', 9000)
+c = GirderClient(host='localhost', port=9000)
 c.authenticate('girder', 'girder')
-folderSearch = c.sendRestRequest('GET', 'resource/search', {
+folderSearch = c.get('resource/search', parameters={
     'q': 'Analyses',
     'types': '["folder"]'
 })
 folderId = folderSearch['folder'][0]['_id']
 
-# Upload our analyses to girder.
-itemId = c.createItem(folderId, 'aceArbor', 'Ancestral state reconstruction')
-c.addMetaDataToItem(itemId, ACR)
-itemId = c.createItem(folderId, 'Phylogenetic signal', 'Phylogenetic signal')
-c.addMetaDataToItem(itemId, PGS)
+# Disable authorization requirements for running romanesco tasks
+c.put('system/setting', parameters={
+    'key': 'romanesco.require_auth',
+    'value': 'false'
+})
+
+# Check if these analyses already exist.  If so, we won't re-upload them.
+uploadACR = False
+uploadPHYSIG = False
+uploadPGLS = False
+
+searchACR = c.get('resource/search', {
+    'q': 'aceArbor',
+    'types': '["item"]'
+})
+if len(searchACR['item']) == 0:
+  uploadACR = True
+
+searchPHYSIG = c.get('resource/search', {
+    'q': 'Phylogenetic signal',
+    'types': '["item"]'
+})
+if len(searchPHYSIG['item']) == 0:
+  uploadPHYSIG = True
+
+searchPGLS = c.get('resource/search', {
+    'q': 'PGLS',
+    'types': '["item"]'
+})
+if len(searchPGLS['item']) == 0:
+  uploadPGLS = True
+
+
+
+# Read our analyses into Python dictionaries and upload them to girder.
+if uploadACR:
+  ACR = {}
+  with open ("%s/ancestral-state/aceArbor.json" % arborWebAppsPath, "r") as acrFile:
+      acrStr = acrFile.read()
+  ACR['analysis'] = json.loads(acrStr)
+  item = c.createItem(folderId, 'aceArbor', 'Ancestral state reconstruction')
+  c.addMetadataToItem(item['_id'], ACR)
+  print "aceArbor successfully uploaded"
+else:
+  print "aceArbor already exists"
+
+if uploadPHYSIG:
+  PHYSIG = {}
+  with open ("%s/phylogenetic-signal/Phylogenetic_signal.json" % arborWebAppsPath, "r") as physigFile:
+      physigStr = physigFile.read()
+  PHYSIG['analysis'] = json.loads(physigStr)
+  item = c.createItem(folderId, 'Phylogenetic signal', 'Phylogenetic signal')
+  c.addMetadataToItem(item['_id'], PHYSIG)
+  print "Phylogenetic signal successfully uploaded"
+else:
+  print "Phylogenetic signal already exists"
+
+if uploadPGLS:
+  PGLS = {}
+  with open ("%s/PGLS/PGLS.json" % arborWebAppsPath, "r") as pglsFile:
+      pglsStr = pglsFile.read()
+  PGLS['analysis'] = json.loads(pglsStr)
+  item = c.createItem(folderId, 'PGLS', 'PGLS')
+  c.addMetadataToItem(item['_id'], PGLS)
+  print "PGLS successfully uploaded"
+else:
+  print "PGLS already exists"
+
